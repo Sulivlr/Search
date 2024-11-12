caf5dfb7fb611c6bd640a890b28f4fac86c4c818
Sultan Mukhtarov <suli419956@gmail.com>
2024-11-12 10:57:49 +0600

    Initial Commit.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Appbar from "./components/AppBar/Appbar";
+
+const App = () => {
+
+  return (
+    <>
+        <header>
+            <Appbar />
+        </header>
+        <main>
+            content
+        </main>
+    </>
+  );
+};
+
+export default App;
